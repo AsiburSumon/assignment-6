fa06673c362ec875b59ff3c73c301cce4e32f4e6
AsiburSumon <sumonasif68@gmail.com>
2022-09-02 23:53:24 +0600

    categories added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('I am connected')
+const loadCategory =()=>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=>displayCategory(data.data.news_category))
+}
+
+const displayCategory=categories=>{
+    const allCategroy = document.getElementById('all-category');
+    categories.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('row','d-flex');
+        categoryDiv.innerHTML=`
+        <a class="mx-3 text-decoration-none text-secondary">${category.category_name}</a>
+        `;
+        allCategroy.appendChild(categoryDiv);
+    });
+}
+
+loadCategory();
