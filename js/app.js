23ed30d39b6bbf29f378fe6c20fd86340df7dd3e
AsiburSumon <sumonasif68@gmail.com>
2022-09-03 17:07:21 +0600

    modals added

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ const displayPerCategory=category1=>{
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML='';
     category1.forEach(news => {
+        const newsId = news._id;
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML=`
@@ -38,11 +39,10 @@ const displayPerCategory=category1=>{
                 <p class="card-text">${news.details.slice(0, 150)+'...'}</p>
                 <div class="d-flex">
                 <img style="height:50px; width:50px;" src="${news.author.img}" class="img-fluid me-2">
-                <p class="fw-bold me-3">${news.author.name}</p>
-                <p class="fw-bold me-3">${news.total_view}</p>
-                <button class="btn btn-danger px-3">Details</button>
+                <p class="fw-bold me-3 text-danger">${news.author.name ? news.author.name:'No Data Found'}</p>
+                <p class="fw-bold me-3">${news.total_view ? news.total_view: 'No Data Found'}</p>
+                <button onclick="loadNewsDetails('${newsId}')" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#newsDetailModal">Details</button>
                 </div>
-                
              </div>
         </div>
         `;
@@ -50,5 +50,27 @@ const displayPerCategory=category1=>{
     });   
 }
 
-// perCategory();
+const loadNewsDetails =(news_id)=>{
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNewsDetails(data.data[0]))
+}
+const displayNewsDetails=data=>{
+    const modalTitle = document.getElementById('newsDetailModalLabel');
+    modalTitle.innerHTML = data.title;
+    const newsDetails = document.getElementById('news-details');
+    newsDetails.innerHTML = `
+        <img src="${data.image_url}" class="img-fluid">
+        <p><span class="fs-5 fw-bold">Details:</span> ${data.details}</p>
+        <p><span class="fs-5 fw-bold">Rating Badge:</span> ${data.rating.badge}</p>
+        <p><span class="fs-5 fw-bold">Rating:</span> ${data.rating.number}</p>
+        <p><span class="fs-5 fw-bold">Is Todays Pick:</span> ${data.others_info.is_todays_pick}</p>
+        <p><span class="fs-5 fw-bold">Is Trending:</span> ${data.others_info.is_trending}</p>
+    `;
+}
+
+
+// loadNewsDetails();
+// displayPerCategory();
 loadCategory();
